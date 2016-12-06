50a20c391afa3fe181972e03d4b3dbde03de3212
t0d0 <hero.td@gmail.com>
2016-12-07 06:03:19 +0900

    諸々の呼び出し

--- main.py
+++ main.py
@@ -3,17 +3,14 @@
 """myapp1.pyw: Create an instance of Ui_Form."""
 # pylint: disable=no-name-in-module
 import sys
-#from PyQt5.QtWidgets import (QApplication, QWidget)
+import json
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
-
-# pyuic5.bat myform.ui > ui_myform.py
-#from ui_myform import Ui_Form
 from ui import Ui_MainWindow
-
-import sys
-import time
+from ui_manager import *
+from serial_manager import *
+#import time
 
 
 class MyApp(Ui_MainWindow):
@@ -24,9 +21,28 @@ class MyApp(Ui_MainWindow):
         Ui_MainWindow.__init__(self)
 
 if __name__ == "__main__":
+    jsonstr = open('.config').read()
+    print(json.loads(jsonstr))
+    
     app = QApplication(sys.argv)
     MainWindow = QMainWindow()
+    
     ui = MyApp()
     ui.setupUi(MainWindow)
     MainWindow.show()
+    
+#   UI_manager
+    ui_m = MainUIManager(ui)
+#   Serial_manager
+    ser_m = SerialArduino()
+    
+#   Set serial_port to combo_box
+    for i in ser_m.get_port_list():
+      ui_m.add_port(i)
+
+#   Set event when clicked send_button
+    ui_m.set_send_action(lambda:ser_m.write_list(ui_m.get_parameters_to_list(),ui_m.get_selected_port()))
+
+  
+#   exit
     sys.exit(app.exec_())
