0a6034d123b4ed1801bb8f8a4c8bf488460a540f
t0d0 <hero.td@gmail.com>
2016-12-09 10:10:09 +0900

    取りあえず動くように色々修正

--- serial_manager.py
+++ serial_manager.py
@@ -2,13 +2,11 @@ import serial
 from serial.tools import list_ports
 import os
 import threading
+import time
 
 class SerialArduino():
   def __init__(self):
-    self.ser = serial.Serial()
-    self.ser.baudrate = 9600
-    self.devices = self.get_port_list()
-    self.selected_port = None
+    pass
 
   def get_port_list(self):
     devices = []
@@ -16,19 +14,47 @@ class SerialArduino():
       devices.append(info.device)
     return(devices)
 
-  def write_list(self,datas,port):
-    self.ser.port = port
+  def write_list(self,datas,port,baudrate = 9600):
+    self.ser = serial.Serial(port,baudrate,timeout=0.1)
+#    self.ser.baudrate = baudrate
+#    self.ser.port = port
     check_thread=threading.Thread(target = lambda:self.check_and_next(datas))
     check_thread.start()
     
   def check_and_next(self,datas):
-    for data in datas:
-      self.ser.readline()#buffer clear
-      
+#    if(self.ser.read(1) == b'\xff'):
+#      print("hoge")
+    for i, data in enumerate(datas):
+      print(data)
+#      try:
+##        self.ser.readline()#buffer clear
+#        pass
+#      except:
+#        print("buffer clear")
+#        pass
       while(1):
-        ser.write(data.to_bytes(1,'big'))
-        time.sleep(0.05)
-        if(ser.read(1) == 255):#arduino answer check
+#        print([(bin(bytes([i]))) for i in range(255)])
+        self.ser.write(bytes([data]))
+#        time.sleep(0.5)
+        print(i + ":wrote")
+        
+#      try:
+        if(self.ser.read(1) == b'\xff'):#arduino answer check
+          print(i + ":arduino ok")
           break
+#        elif(self.ser.read(1) == b'\x00'):
+#          print("retry")
+#          pass
         else:
+#          time.sleep(0.5)
+          print(i + ":retry")
           pass
+#      except:
+#        time.sleep(0.5)
+#        print("except")
+#        pass
+    print("Completed writing of config data.")
+    self.ser.close()
+    
+#  def test(self):
+#    self.ser = serial.Serial(port,baudrate)
